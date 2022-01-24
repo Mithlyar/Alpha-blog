5a16012d607fef4fa6c28ac5d52258354edf5aa9
Arek Sowa <Areksowa26@gmail.com>
2022-01-24 20:13:17 +0100

    DRY-ing written code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 def show
-  @article = Article.find(params[:id])
 end
 
 def index
@@ -13,12 +13,11 @@ def new
 end
 
 def edit
-@article = Article.find(params[:id])
 end
 
 def create
 
-@article  = Article.new(params.require(:article).permit(:title, :desription))
+@article  = Article.new(article_params)
 if @article.save
   flash[:notice] = "Article was created succesfully!"
  redirect_to (@article)
@@ -28,8 +27,7 @@ end
 end
 
 def update
-@article = Article.find(params[:id])
-if @article.update(params.require(:article).permit(:title, :desription))
+if @article.update(article_params)
   flash[:notice] = "Article was updated succesfully!"
   redirect_to (@article)
 else
@@ -38,9 +36,17 @@ end
 end
 
 def destroy
-  @article = Article.find(params[:id])
   @article.destroy
   redirect_to articles_path
 end
+
+private
+def set_article
+@article = Article.find(params[:id])
+end
+
+ def article_params
+   params.require(:article).permit(:title, :desription)
+ end
 
 end
